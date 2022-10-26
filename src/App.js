705c341926c05b1d3018de8371f7298277d9a24b
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-10-26 02:45:23 +0200

    clean up react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-
+import React from "react";
 import './App.css';
+import HomeScreen from "./HomeScreen";
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello World</h1>
-      {/* <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <div className="app">
+    <HomeScreen />
     </div>
   );
 }
