d328ae686a8b6cdcce773fe2d46ce8ae8f8a6a65
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-11-01 02:12:09 +0200

    react app

---
+++ netflix-clone-react/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAcNI36vt383ATXIQcA-ZTcLNPHydSxPEA",
+  authDomain: "netflix-clone-react-d16e4.firebaseapp.com",
+  projectId: "netflix-clone-react-d16e4",
+  storageBucket: "netflix-clone-react-d16e4.appspot.com",
+  messagingSenderId: "126303341719",
+  appId: "1:126303341719:web:ebe96f873a1f41404b2dd2",
+};
