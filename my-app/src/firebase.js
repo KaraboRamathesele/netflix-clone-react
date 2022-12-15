935df015e206bdbf7e867383d0441de4396d5d9d
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-12-15 18:06:56 +0200

    react app is working

---
+++ my-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGhiDNBGXa_cCyanObqH_ottg_u8vEMWM",
+  authDomain: "my-app-45113.firebaseapp.com",
+  projectId: "my-app-45113",
+  storageBucket: "my-app-45113.appspot.com",
+  messagingSenderId: "980125510111",
+  appId: "1:980125510111:web:f4ac9c9a8b840d22bafc6a",
+  measurementId: "G-64CNS065VR",
+};
