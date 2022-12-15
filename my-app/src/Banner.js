511bc2dcecc7c0ceac5c849dca101f4c4b897070
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-12-16 00:13:01 +0200

    banner set up

---
+++ my-app/src/Banner.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://occ-0-34-32.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABetr-B2Dv47ohLycEbEyx5PIe-sdPZW4iATGQnsJ1zOeCTrb-OHrsx4oLnBwbnhWQTuTd35xjIgbwE2WmobNSH_z1s4eLLgKLuNf.webp?r=109')`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <img src="" alt="" />
+    </header>
+  );
+}
+
+export default Banner;
