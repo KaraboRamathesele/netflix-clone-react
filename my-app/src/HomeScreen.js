e887cead2c7139d1230abb791d7f682205aed252
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-12-15 23:55:36 +0200

    Creating evenet listener for navbar bckgrnd fade

--- my-app/src/HomeScreen.js
+++ my-app/src/HomeScreen.js
@@ -1,10 +1,33 @@
 import React from "react";
 import "./HomeScreen.css";
+import Nav from "./Nav";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
-      {/* Nav */}
+      <Nav />
+
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
 
       {/* Banner */}
 
