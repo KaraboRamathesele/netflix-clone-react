06693e5ecfd0314212d1c0208bc14263bb497dc6
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-12-16 02:03:31 +0200

    row.js

--- my-app/src/HomeScreen.js
+++ my-app/src/HomeScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "./Banner";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import requests from "../Requests";
 
 function HomeScreen() {
   return (
@@ -10,7 +11,18 @@ function HomeScreen() {
 
       <Banner />
 
-      {/* Row */}
+      <Row
+        title="NETFLIX ORGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
